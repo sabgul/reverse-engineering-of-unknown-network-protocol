e74599091c75ea014527d1ff117a89ccfbc7e185
sabinathe <41567711+sabgul@users.noreply.github.com>
2021-11-14 17:33:13 +0100

    progress

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -14,6 +14,7 @@
 #include <vector>
 #include <algorithm>
 #include <arpa/inet.h>
+#include <fstream>
 
 #include <unistd.h>
 #include <errno.h>
@@ -22,10 +23,12 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <regex>
 
 #define SUCCESS 0
 #define ARG_ERROR 1
 #define ERROR 2
+#define SERVER_ERROR -1
 #define INTERNAL_ERR 99
 
 //TODO na toto sa pozriet a nadefinovat to lepsie
@@ -39,13 +42,9 @@
 struct user {
     std::string login;
     std::string passwd_hash;
-}; 
-
-struct logged_user {
-    std::string login;
-    std::string passwd_hash;
-    std::string session_hash;
 };
+
+//TODO kontrola, ci je user uz logged in a registered je zbytocna, pretoze to mi povie server
 
 /**
  * @brief Function displays help and usage of the script if -h or --help option was used
@@ -84,37 +83,104 @@ bool in_array(const std::string &value, const std::vector<std::string> &array)
     return std::find(array.begin(), array.end(), value) != array.end();
 }
 
-
-void *get_in_addr(struct sockaddr *sa)
+// TODO prerobit a ocitovat
+char* base64Encoder(char input_str[], int len_str)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+   
+    // Character set of base64 encoding scheme
+    char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+      
+    // Resultant string
+    char *res_str = (char *) malloc(SIZE * sizeof(char));
+      
+    int index, no_of_bits = 0, padding = 0, val = 0, count = 0, temp;
+    int i, j, k = 0;
+      
+    // Loop takes 3 characters at a time from
+    // input_str and stores it in val
+    for (i = 0; i < len_str; i += 3)
+        {
+            val = 0, count = 0, no_of_bits = 0;
+  
+            for (j = i; j < len_str && j <= i + 2; j++)
+            {
+                // binary data of input_str is stored in val
+                val = val << 8;
+                  
+                // (A + 0 = A) stores character in val
+                val = val | input_str[j];
+                  
+                // calculates how many time loop
+                // ran if "MEN" -> 3 otherwise "ON" -> 2
+                count++;
+              
+            }
+  
+            no_of_bits = count * 8;
+  
+            // calculates how many "=" to append after res_str.
+            padding = no_of_bits % 3;
+  
+            // extracts all bits from val (6 at a time)
+            // and find the value of each block
+            while (no_of_bits != 0)
+            {
+                // retrieve the value of each block
+                if (no_of_bits >= 6)
+                {
+                    temp = no_of_bits - 6;
+                      
+                    // binary of 63 is (111111) f
+                    index = (val >> temp) & 63;
+                    no_of_bits -= 6;        
+                }
+                else
+                {
+                    temp = 6 - no_of_bits;
+                      
+                    // append zeros to right if bits are less than 6
+                    index = (val << temp) & 63;
+                    no_of_bits = 0;
+                }
+                res_str[k++] = char_set[index];
+            }
     }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  
+    // padding is done here
+    for (i = 1; i <= padding; i++)
+    {
+        res_str[k++] = '=';
+    }
+  
+    res_str[k] = '\0';
+  
+    return res_str;
 }
 
+/**
+ * @brief Function splits the input string into tokens separated by delimiter
+ * 
+ * @param s - string to be split
+ * @param delim - delimiter splitting the tokens
+ * @return std::vector<std::string> - vector of tokens
+ * 
+ * @ref https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+ */
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss (s);
+    std::string item;
 
-// int processArguments(int argc, char **argv) {
-//     return SUCCESS;
-// }
-
-// void printShit(int &num) {
-//     std::cout << num;
-// }
-
-// template<typename T>
-// T sumMyVec(std::vector<T> input) {
-//     T sum = 0;
-//     std::for_each(input.cbegin(), input.cend(), [&](T n){ sum += n; });
-
-//     return sum;
-// }
+    while(getline (ss, item, delim)) {
+        result.push_back(item);
+    }
+    return result;
+}
 
 int main (int argc, char **argv) {
     /* ------------------------- PROCESSING OF CMD ARGUMENTS ------------------------- */
     int nCommands = 0;
-    std::vector<std::string> commands {"register", "login", "send", "list", "fetch"};
+    std::vector<std::string> commands {"register", "login", "send", "list", "fetch", "logout"};
 
     int port = 32323;
     std::string address = "127.0.0.1";
@@ -123,10 +189,9 @@ int main (int argc, char **argv) {
     std::string outgoing_message;
 
     std::vector<user> registered_users;
-    std::vector<logged_user> logged_users;
+    std::string login_hash;
+    std::string final_login_hash;
 
-    // user emil {"emil", "0000"};
-    // registered_users.push_back(emil);
     if(argc == 1) {
         std::cerr << "error: client expects <command> [<args>] ... \n       0 arguments given \n       see '-h', or '--help'" << "\n";
         return ARG_ERROR;
@@ -142,7 +207,9 @@ int main (int argc, char **argv) {
             nCommands++;
             command = argv[i];
 
+            
             if(strcmp(argv[i], "register" ) == 0) {
+            /* ------------------------- REGISTER ------------------------- */
                 // check valid num of args - if argc > i + 2
                 if(i+2>=argc) {
                     std::cerr << "error: invalid number of arguments for command register" << "\n";
@@ -151,7 +218,6 @@ int main (int argc, char **argv) {
 
                 std::string incoming_login = argv[i+1];
                 std::string incoming_passwd = argv[i+2];
-                std::cout << "Pass before encoding: " << incoming_passwd << "\n";
 
                 // TODO encode to base64
                 
@@ -162,14 +228,18 @@ int main (int argc, char **argv) {
                         return ERROR;
                     }
                 }
-
+                int len_str = incoming_passwd.length();
+                char pass_str[len_str + 1];
+                strcpy(pass_str, incoming_passwd.c_str());
+                incoming_passwd = base64Encoder(pass_str, len_str);
                 // create new user and add it to vector registered users
                 user User = {incoming_login, incoming_passwd};
                 registered_users.push_back(User);
-
+                
                 // concatenate the strings into message for server (register "login" "base64passwd")
                 outgoing_message = "(" + command + " \"" + incoming_login + "\" " + "\"" + incoming_passwd + "\"" + ")";
             } else if(strcmp(argv[i], "login" ) == 0) {
+                /* ------------------------- LOGIN ------------------------- */
                 if(i+2>=argc) {
                     std::cerr << "error: invalid number of arguments for command login" << "\n";
                     return ARG_ERROR;
@@ -178,21 +248,42 @@ int main (int argc, char **argv) {
                 std::string incoming_login = argv[i+1];
                 std::string incoming_passwd = argv[i+2];
                 // TODO encode to base64
-                // TODO najskor treba poslat outgoing message a zistit od serveru session hash, potom takehoto usera vytvorit a pridat do tabulky
-                // check if user with same login argv[i+1] is already in table
-                for(std::vector<logged_user>::iterator i = logged_users.begin(); i != logged_users.end(); ++i) {
-                    if(strcmp(((*i).login).c_str(), incoming_login.c_str()) == 0) {
-                        std::cerr << "ERROR: user already logged in" << "\n";
-                        return ERROR;
-                    }
-                } 
-
-                logged_user User = {incoming_login, incoming_passwd, "Unknown"}; //TODO session hash
-                logged_users.push_back(User);
+                int len_str = incoming_passwd.length();
+                char pass_str[len_str + 1];
+                strcpy(pass_str, incoming_passwd.c_str());
+                incoming_passwd = base64Encoder(pass_str, len_str);
 
                 outgoing_message = "(" + command + " \"" + incoming_login + "\" " + "\"" + incoming_passwd + "\"" + ")";
                 std::cout << outgoing_message << "\n";
-            }
+
+            } else if(strcmp(argv[i], "send" ) == 0) {
+            /* ------------------------- SEND ------------------------- */
+                if(i+3>=argc) {
+                    std::cerr << "error: invalid number of arguments for command login" << "\n";
+                    return ARG_ERROR;
+                }
+
+                std::ifstream t("login-token");
+                std::stringstream login_hash_token;
+                login_hash_token << t.rdbuf();
+
+                outgoing_message = "(" + command + " \"" + login_hash_token.str() + "\"" + " \"" + argv[i+1] + "\"" + " \"" + argv[i+2] + "\"" + " \"" +argv[i+3] + "\"" +")";
+            } else if(strcmp(argv[i], "list") == 0) {
+                std::ifstream t("login-token");
+                std::stringstream login_hash_token;
+                login_hash_token << t.rdbuf();
+                
+                outgoing_message = "(" + command + " \"" + login_hash_token.str() + "\"" + ")";
+            } else if(strcmp(argv[i], "fetch") == 0) {
+
+            } else if(strcmp(argv[i], "logout") == 0) {
+            /* ------------------------ LOGOUT ------------------------ */
+                std::ifstream t("login-token");
+                std::stringstream login_hash_token;
+                login_hash_token << t.rdbuf();
+
+                outgoing_message = "(" + command + " \"" + login_hash_token.str() + "\"" + ")";
+            } 
         } 
 
         /* --port */
@@ -229,7 +320,7 @@ int main (int argc, char **argv) {
         /* send */
         /* list */
         /* fetch */
-
+        /* logout */
     } 
 
     if(nCommands > 1) {
@@ -241,80 +332,83 @@ int main (int argc, char **argv) {
     }
     /* ------------------------------------------------------------------------------- */
 
-    // int sockfd, numbytes;  
-    // char buf[MAXDATASIZE];
-    // struct addrinfo hints, *servinfo, *p;
-    // int rv;
-    // char s[INET6_ADDRSTRLEN];
+    // TODO OCITOVAT
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "error: socket creation error." << "\n";
+        return ERROR; 
+    } 
 
-    // memset(&hints, 0, sizeof hints);
-    // hints.ai_family = AF_UNSPEC;
-    // hints.ai_socktype = SOCK_STREAM;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORTNUM);
 
-    // if ((rv = getaddrinfo(ADDRESSSTR, PRT, &hints, &servinfo)) != 0) {
-    //     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    //     return 1;
-    // }
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        std::cerr << "error: address error." << "\n";
+        return ERROR; 
+    }
 
+    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "error: connection failed." << "\n";
+        return ERROR; 
+    }
 
-    // if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-    //         displayHelp();
-    //         return SUCCESS;
-    // }
-    // int a = 10;
-    // int b = 5;
-    
-    // std::string ahoj = "Ahojky";
-    // std::string meno = " Abika";
+    send(sock, outgoing_message.c_str(), outgoing_message.length(), 0);
+    std::cout << "Message sent: " << outgoing_message << "\n";
 
-    // std::string pozdrav = ahoj + meno;
-    // std::cout << pozdrav;
+    valread = read(sock, buffer, 1024);
+    std::cout << "Message received: " << buffer << "\n";
 
-    // std::vector<int> skapvec1 = {1, 2, 3, 4, 5};
-    // std::vector<float> skapvec2 = {1.1f, 2.f, 3.1f, 4.32f, 5.23f};
-    
-    // int sum1 = sumMyVec(skapvec1);
-    // float sum2 = sumMyVec(skapvec2);
+    if(strcmp(command.c_str(), "register" ) == 0) {
+        std::vector<std::string> response = split(buffer, '\"');
 
-    // std::cout << "before:";
-    // std::for_each(skapvec.cbegin(), skapvec.cend(), [](int n){ printShit(n); });
-    // std::cout << '\n';
-    // for(p = servinfo; p != NULL; p = p->ai_next) {
-    //     if ((sockfd = socket(p->ai_family, p->ai_socktype,
-    //             p->ai_protocol)) == -1) {
-    //         perror("client: socket");
-    //         continue;
-    //     }
+        if(strcmp(response.at(0).c_str() ,"(ok ") == 0) {
+            std::cout << "SUCCESS: "<< response.at(1) << "\n";
+        } else {
+            std::cout << "ERROR: "<< response.at(1) << "\n";
+        }
+    } else if(strcmp(command.c_str(), "login" ) == 0) {
+        /* splits the response from server into chunks conatining login token and state of operation*/
+        std::vector<std::string> response = split(buffer, '\"');
 
-    //     if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-    //         close(sockfd);
-    //         perror("client: connect");
-    //         continue;
-    //     }
+        if(strcmp(response.at(0).c_str() ,"(ok ") == 0) {
+            std::cout << "SUCCESS: "<< response.at(1) << "\n";
+            login_hash = response.at(3);
+            std::ofstream tokenFile;
+            tokenFile.open("login-token");
+            tokenFile << login_hash;
+            tokenFile.close();
+        } else {
+            std::cout << "ERROR: "<< response.at(1) << "\n";
+        }
+    } else if(strcmp(command.c_str(), "send" ) == 0) {
+        std::vector<std::string> response = split(buffer, '\"');
 
-    //     break;
-    // }
+        if(strcmp(response.at(0).c_str() ,"(ok ") == 0) {
+            std::cout << "SUCCESS: "<< response.at(1) << "\n";
+        } else {
+            std::cout << "ERROR: "<< response.at(1) << "\n";
+        }
+    } else if(strcmp(command.c_str(), "list" ) == 0) {
+        //TODO add all listed messages into structure ?? parse them and then display
+        std::vector<std::string> response = split(buffer, ' ');
+        if(strcmp(response.at(0).c_str() ,"(ok") == 0) {
+            std::cout << "SUCCESS: \n";
+        } else {
+            std::cout << "ERROR: listing of sent messages failed.\n";
+            return ERROR;
+        }
+    } else if(strcmp(command.c_str(), "fetch" ) == 0) {
 
-    // if (p == NULL) {
-    //     fprintf(stderr, "client: failed to connect\n");
-    //     return 2;
-    // }
+    } else if ((strcmp(command.c_str(), "logout" ) == 0)) {
+        std::vector<std::string> response = split(buffer, '\"');
+        if(strcmp(response.at(0).c_str() ,"(ok ") == 0) {
+            std::cout << "SUCCESS: "<< response.at(1) << "\n";
+        } else {
+            std::cout << "ERROR: "<< response.at(1) << "\n";
+        }
+    }
 
-    // inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-    //         s, sizeof s);
-    // printf("client: connecting to %s\n", s);
-
-    // freeaddrinfo(servinfo); // all done with this structure
-
-    // if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-    //     perror("recv");
-    //     exit(1);
-    // }
-
-    // buf[numbytes] = '\0';
-
-    // printf("client: received '%s'\n",buf);
-
-    // close(sockfd);
     return SUCCESS;
 }
